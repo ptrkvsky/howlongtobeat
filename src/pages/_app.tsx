115281d6908f3295ded301c093021fe86f08ced0
ptrkvsky <johan.petrikovsky@gmail.com>
2022-01-12 18:57:50 +0100

    fix: display console.log only client side

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,17 +6,18 @@ import StyleContainer from '@/styles/containers/StyleContainer';
 import { siteInformations } from '@/config/siteInformations';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const styles = [
-    `color: white`,
-    `background: #1a1a1a`,
-    `font-size: 64px`,
-    `font-family: white`,
-    `padding: 6px 8px`,
-  ].join(`;`);
-  const message = `GAME-OVER.IO`;
-
-  // eslint-disable-next-line no-console
-  console.info(`%c%s`, styles, message);
+  if (typeof window !== `undefined`) {
+    const styles = [
+      `color: white`,
+      `background: #1a1a1a`,
+      `font-size: 64px`,
+      `font-family: white`,
+      `padding: 6px 8px`,
+    ].join(`;`);
+    const message = `GAME-OVER.IO`;
+    // eslint-disable-next-line no-console
+    console.info(`%c%s`, styles, message);
+  }
 
   useEffect(() => {
     TagManager.initialize({ gtmId: siteInformations.tagManager });
